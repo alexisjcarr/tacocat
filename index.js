3e0a15924ba459a3a49a1264b600106f56804328
alexisjcarr <acarr59@gmail.com>
2019-05-04 17:23:57 -0500

    Added yelp api call

--- index.js
+++ index.js
@@ -1,22 +1,48 @@
+const https = require("https");
+
 // Yelp API querying
 const clientKey = 'Tk50ZkP7y0dKQSX3FZ77Bw';
 const apiKey = 'rJuKdTen_ktUx0Mls6culJDA0P3yXXP5FoajDMiozWFFJOcxfgNejA-LL5ejEt17ihVzEvduWdvVs9Nmb-RD0IIyZwOfiHPl415Rm2t-tS1-8Ezyat6J1f3PWoZfXHYx';
 
-const request = new XMLHttpRequest();
-request.open('GET', `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=tacos`);
-request.setRequestHeader(Authorization, `Bearer ${apiKey}`);
+// const request = new XMLHttpRequest();
+// request.open('GET', `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=tacos`);
+// request.setRequestHeader(Authorization, `Bearer ${apiKey}`);
 
-request.onload = function () {
-    const data = JSON.parse(this.response);
+// request.onload = function () {
+//     const data = JSON.parse(this.response);
 
-    if(request.status >= 200 && request.status < 400) {
-        data.forEach(taqueria => {
-            console.log(taqueria)
-        })
-    } else {
-        console.log('error')
+//     if(request.status >= 200 && request.status < 400) {
+//         data.forEach(taqueria => {
+//             console.log(taqueria)
+//         })
+//     } else {
+//         console.log('error')
+//     }
+
+// }
+
+// request.send()
+
+const lat =  30.588059799999996;
+const long = -96.3230157;
+
+const options = {
+    hostname: 'api.yelp.com',
+    path: `/v3/businesses/search?term=tacos&latitude=${lat}&longitude=${long}`,
+
+    headers: {
+        Authorization: `Bearer ${apiKey}`
     }
-
 }
 
-request.send()
+https.get(options, (res) => {
+  console.log('statusCode:', res.statusCode);
+  console.log('headers:', res.headers);
+
+  res.on('data', (d) => {
+    console.log(d);
+  });
+
+}).on('error', (e) => {
+  console.error(e);
+});
